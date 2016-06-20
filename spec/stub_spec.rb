4775776f30232e9c4ec8275758780d951ec8e6f5
imontalvomiguel <ubaldo.im@gmail.com>
2016-06-20 15:45:51 -0500

    add comments for stubs

--- spec/stub_spec.rb
+++ spec/stub_spec.rb
@@ -16,14 +16,19 @@ class PriceCalculator
   end
 end
 
+# We used stubs to simulate some state
 describe "Stubs" do
   it "privedes stubs to simulate state" do
     calculator = PriceCalculator.new
-    product = double("Product")
-    allow(product).to receive_messages(:price => 1.0)
 
-    calculator.add(product)
+    # for example, instantiating a new "Product"
+    product_stub = double("Product")
+    # and simulating the :price message in order to have a particular value
+    allow(product_stub).to receive(:price).and_return(1.0, 100.25)
+
+    2.times { calculator.add(product_stub) }
     calculator.add double(price: 100.25)
-    expect(calculator.final_price).to eq 101.25
+
+    expect(calculator.final_price).to eq 201.5
   end
 end
